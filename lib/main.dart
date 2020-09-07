010eaf41f67f318bfa19e089e0f6781d5b011c00
juntot <junrey.gonzales.07@gmail.com>
2020-09-07 09:42:40 +0800

    .

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,14 @@ void main()=>runApp(
   )
 );
 
-class Home extends StatelessWidget {
-  final int count = 0;
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +22,42 @@ class Home extends StatelessWidget {
         style: TextStyle(fontWeight: FontWeight.bold),),
         backgroundColor: Colors.red,
       ),
-      body: Center(
-        child: Text('$count')
+      body: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    image:  DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1540206395-68808572332f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2081&q=80"), fit: BoxFit.cover,),
+                  ),
+                ),
+                Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Tae',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white
+                        ),
+
+                    ),
+                    Text(count.toString(),
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'kaushan',
+                      color: Colors.white
+                    ),)
+                  ],
+                )
+              ),
+              ]
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Add',
-        child: Text('Add'),
+        child: Icon(Icons.plus_one),
         onPressed:(){
-
+          setState(() {
+            count+=1;
+          });
         },
         backgroundColor: Colors.red
       ),
